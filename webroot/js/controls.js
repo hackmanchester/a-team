0ed6f5460c81ffd73390e02374c57d9e3faad35d
Mike Pearce <mike@mikepearce.net>
2012-10-28 00:01:45 +0100

    Deleted some key events

--- webroot/js/controls.js
+++ webroot/js/controls.js
@@ -24,31 +24,6 @@ $(function(){
         _emitControlEvent(socket, this, 'stop');
     });
 
-    $(document).keydown(function(e) {
-        var keyCode = e.keyCode || e.which,
-        arrow = {left: 37, up: 38, right: 39, down: 40, z: 122, x: 120 };
-        switch (keyCode) {
-            case arrow.left:
-                _emitControlEvent(socket, {}, 'start', {type:"move",action:"left"});
-            break;
-            case arrow.up:
-                _emitControlEvent(socket, {}, 'start', {type:"move",action:"up"});
-            break;
-            case arrow.right:
-                _emitControlEvent(socket, {}, 'start', {type:"move",action:"right"});
-            break;
-            case arrow.down:
-                _emitControlEvent(socket, {}, 'start', {type:"move",action:"down"});
-            break;
-            case arrow.z:
-                _emitControlEvent(socket, {}, 'start', {type:"shoot",action:"bullet"});
-            break;
-            case arrow.x:
-                _emitControlEvent(socket, {}, 'start', {type:"shoot",action:"mine"});
-            break;
-        }
-    });
-
     // Shoot events
     $('div[data-type=shoot]').mousedown(function(){
         _emitControlEvent(socket, this, 'start');
