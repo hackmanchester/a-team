94b3689e844c5f1aff48760e7d7b063f06affcf7
Yarek T <yarekt@gmail.com>
2012-10-27 18:30:04 +0100

    Add main and load event

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 var io = require('socket.io').listen(9000);
+io.set('log level', 5);
 
 io.sockets.on('connection', function (socket) {
+    // Send everyone's position to bootstrap client
+    socket.on('load', function() {
+        console.log('Client connected, send map to them');
+    });
 
+    // Main event distribution event =D
+    socket.on('tank', function(data) {
+        socket.broadcast.event('tank', data);
+    });
 });
